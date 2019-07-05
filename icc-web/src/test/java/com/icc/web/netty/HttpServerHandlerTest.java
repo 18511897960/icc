a4b7fe536fb31ecb8fd654f43059d91e9346197a
18511897960 <924225226@qq.com>
2019-07-05 17:33:34 +0800

    l
    l

---
+++ icc-web/src/test/java/com/icc/web/netty/HttpServerHandlerTest.java
@@ -0,0 +1,12 @@
+package com.icc.web.netty;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HttpServerHandlerTest {
+
+    @Test
+    public void channelRead0() {
+    }
+}
